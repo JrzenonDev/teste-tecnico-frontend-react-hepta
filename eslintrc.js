7c305bc62b9b889dff3c136b9433f20a0634481c
JrzenonDev <jrobertoonb@gmail.com>
2025-02-19 16:03:56 -0300

    (feat)[feature/main] install packeges prettier and eslint and config files

---
+++ eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-inline-comments': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
