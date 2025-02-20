f6e62bde918be6482f3348ae3287f96f1dbe26f7
JrzenonDev <jrobertoonb@gmail.com>
2025-02-19 22:11:09 -0300

    (feat)[feature/main] init structure Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
-"use client";
+'use client';
 
-import { Button } from "@mui/material";
+import { Paper, styled } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { CustomBox } from './style';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  boxShadow: theme.shadows[2],
+}));
 
 export default function Home() {
   return (
     <main>
-      <div>
-        Hello world!
-        <Button variant="text">Text</Button>
-      </div>
+      <CustomBox>
+        <Grid container spacing={2} sx={{ width: '100%', margin: '0 auto' }}>
+          <Grid size={8}>
+            <Item>size=8</Item>
+          </Grid>
+          <Grid size={4}>
+            <Item>size=4</Item>
+          </Grid>
+          <Grid size={4}>
+            <Item>size=4</Item>
+          </Grid>
+          <Grid size={8}>
+            <Item>size=8</Item>
+          </Grid>
+        </Grid>
+      </CustomBox>
     </main>
   );
 }
