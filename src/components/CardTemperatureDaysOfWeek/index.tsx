d2d1c73947dd74c5aaf7067b0332dacfff29503c
JrzenonDev <jrobertoonb@gmail.com>
2025-02-20 16:31:59 -0300

    (feat)[feature/main] init component CardTemperatureDaysOfWeek

---
+++ src/components/CardTemperatureDaysOfWeek/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export function CardTemperatureDaysOfWeek() {
+  return (
+    <Box>
+      <Typography>SUN</Typography>
+      <Typography>ICON</Typography>
+      <Typography>20C</Typography>
+      <Typography>10C</Typography>
+    </Box>
+  );
+}
