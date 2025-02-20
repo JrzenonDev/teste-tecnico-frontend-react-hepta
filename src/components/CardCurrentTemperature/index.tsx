04391dc26c3026f939e3f6283a51955fd6e8fc3a
JrzenonDev <jrobertoonb@gmail.com>
2025-02-19 22:35:21 -0300

    (feat)[feature/main] init component CardCurrentTemperature

---
+++ src/components/CardCurrentTemperature/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material';
+import { MapPin } from 'lucide-react';
+
+export function CardCurrentTemperature() {
+  return (
+    <Box>
+      <Typography>PRESSURE: 1021hPA</Typography>
+      <Typography>SUNRISE: 12:37</Typography>
+      <Typography>SUNSET: 12:37</Typography>
+      <Typography>12:37</Typography>
+    </Box>
+  );
+}
