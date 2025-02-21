30993955a950847a0514b03bbc430e62499ae070
JrzenonDev <jrobertoonb@gmail.com>
2025-02-21 09:42:34 -0300

    (feat)[feature/main]adjusts in structure CardCityInformation component

--- src/components/CardCityInformation/index.tsx
+++ src/components/CardCityInformation/index.tsx
@@ -1,13 +1,17 @@
 import { Box, Typography } from '@mui/material';
 import { MapPin } from 'lucide-react';
+import { CustomShadowBox } from '../CustomShadowBox';
 
 export function CardCityInformation() {
   return (
-    <Box>
-      <MapPin /> City
-      <Typography>MAY 5</Typography>
-      <Typography>12:37</Typography>
+    <CustomShadowBox>
+      <Box sx={{ display: 'flex' }}>
+        <MapPin />
+        <Typography>City</Typography>
+      </Box>
+      <Typography variant="h6">MAY 5</Typography>
+      <Typography variant="h5">12:37</Typography>
       <Typography>CLOUD</Typography>
-    </Box>
+    </CustomShadowBox>
   );
 }
