58da1f0c0371455cb32ae5c20fdc3a3316defe85
JrzenonDev <jrobertoonb@gmail.com>
2025-02-21 09:43:55 -0300

    (feat)[feature/main]using CustomShadowBox

--- src/components/CardCurrentTemperature/index.tsx
+++ src/components/CardCurrentTemperature/index.tsx
@@ -1,13 +1,13 @@
-import { Box, Typography } from '@mui/material';
-import { MapPin } from 'lucide-react';
+import { Typography } from '@mui/material';
+import { CustomShadowBox } from '../CustomShadowBox';
 
 export function CardCurrentTemperature() {
   return (
-    <Box>
+    <CustomShadowBox>
       <Typography>PRESSURE: 1021hPA</Typography>
       <Typography>SUNRISE: 12:37</Typography>
       <Typography>SUNSET: 12:37</Typography>
-      <Typography>12:37</Typography>
-    </Box>
+      <Typography variant="h5">20 C</Typography>
+    </CustomShadowBox>
   );
 }
